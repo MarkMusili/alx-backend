e47c5a08e14160640bda8777c90d83d159b48546
MarkMusili <musiliyrn@gmail.com>
2024-04-09 20:24:16 +0300

    Default language fix

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -15,7 +15,7 @@ class Config:
     Config class for Babel
     """
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "fr"
+    BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
