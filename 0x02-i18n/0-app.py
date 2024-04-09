7666420a9eb7e8a67e5fd4615d87245ee9e3b88a
MarkMusili <musiliyrn@gmail.com>
2024-04-09 19:00:37 +0300

    Type annotation

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -2,13 +2,14 @@
 """
 Flask app implimentation
 """
+from typing import Any
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
+def index() -> Any:
     return render_template('0-index.html')
 
 
